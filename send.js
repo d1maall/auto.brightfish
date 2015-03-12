02567c8cb9eb83574162ae0a446369009d80d8f3
d1maall <d1maall1606@gmail.com>
2015-03-12 16:10:38 +0300

    вынес функцию отправки в отдельный файл

---
+++ send.js
@@ -0,0 +1,33 @@
+/**
+ * Created by dmitry on 12.03.15.
+ */
+
+function send() {
+    var name = $('.name').val();
+    var email = $('.email').val();
+    var phone = $('.phone').val();
+    $.post(
+        "./mail.php",
+        {
+            name : $('.name').val(),
+            phone : $('.phone').val(),
+            email: $('.email').val()
+        },
+        onAjaxSuccess
+    );
+    function onAjaxSuccess(data)
+    {
+        // Здесь мы получаем данные, отправленные сервером и выводим их на экран.
+        //alert(data);
+    }
+    //alert('Имя: ' + name + '\n' + 'Почта: ' + email + '\n' + 'Телефон: ' + phone );
+    //немножко говнокода
+
+    alert("Ваша заявка получена. Мы скоро свяжемся с вами.")
+    $('.name').val('');
+    $('.email').val('');
+    $('.phone').val('');
+
+    $.magnificPopup.close();
+
+}
