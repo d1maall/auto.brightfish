79ebad99edcc51dbe2b1f749c689d5c22bcb2c28
d1maall <d1maall1606@gmail.com>
2015-03-12 20:00:02 +0300

    поправил формы

--- send.js
+++ send.js
@@ -7,30 +7,60 @@ function send() {
     //var email = $('.email').val();
     //var phone = $('.phone').val();
 
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    function validatePhone(phone) {
+        var re = /^(?=.*[0-9])[- +()0-9]+$/;
+        return re.test($('.phone').val());
+    }
+
+    $('titleform').text(validatePhone($('.phone').val()));
+
+    if ($('.name').val() == "") {
+        $(".dialogue-name").addClass("dialogue-alert");
+        $(".dialogue-name").text("Введите имя");
+        var namebool = false;
+    }
+    else {var namebool = true}
+
+    if (($('.email').val() == "")||(!validateEmail($('.email').val()))) {
+        $(".dialogue-email").addClass("dialogue-alert");
+        $(".dialogue-email").text('Введите e-mail');
+        var emailbool = false;
+    }
+    else {var emailbool = true}
+
+    if (($('.phone').val() == "")||(!validatePhone($('.phone').val()))) {
+        $(".dialogue-phone").addClass("dialogue-alert");
+        $(".dialogue-phone").text("Введите номер телефона");
+        var phonebool = false;
+    }
+    else {var phonebool = true}
+
     yaCounter26346798.reachGoal('AUTO');
 
-    $.post(
-        "./mail.php",
-        {
-            name : $('.name').val(),
-            phone : $('.phone').val(),
-            email: $('.email').val()
-        },
-        onAjaxSuccess
-    );
-    function onAjaxSuccess(data)
-    {
-        // Здесь мы получаем данные, отправленные сервером и выводим их на экран.
-        //alert(data);
+    if ((namebool) && (emailbool) && (phonebool)) {
+
+        $.post(
+            "./mail.php",
+            {
+                name: $('.name').val(),
+                phone: $('.phone').val(),
+                email: $('.email').val()
+            }
+        );
+        //alert('Имя: ' + name + '\n' + 'Почта: ' + email + '\n' + 'Телефон: ' + phone );
+        alert("Ваша заявка получена. Мы скоро свяжемся с вами.")
+        $('.name').val('');
+        $('.email').val('');
+        $('.phone').val('');
+        $.magnificPopup.close();
     }
-    //alert('Имя: ' + name + '\n' + 'Почта: ' + email + '\n' + 'Телефон: ' + phone );
-    //немножко говнокода
-
-    alert("Ваша заявка получена. Мы скоро свяжемся с вами.")
-    $('.name').val('');
-    $('.email').val('');
-    $('.phone').val('');
-
-    $.magnificPopup.close();
+    else {
+        console.log("error");
+    }
 
 }
